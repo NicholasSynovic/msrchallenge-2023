aecc34a75021b55abd9eadb2b7a8925614445e34
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-07-12 08:50:23 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,13 @@
+# from torchvision import models as visionModels
+# from torchtext import models as textModels
+# from torchaudio import pipelines as audioModels
+
+def main()  ->  None:
+    # models: list[str] = visionModels.list_models()
+
+    foo: str
+    for foo in models:
+        print(foo)
+
+if __name__ == "__main__":
+    main()
