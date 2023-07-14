c179db47df8b7b3443c2504b6d73d69c093096c1
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-07-14 13:37:50 -0500

    Foramt code

--- msrchallenge_2023/countProjects/countProjectsInCSV.py
+++ msrchallenge_2023/countProjects/countProjectsInCSV.py
@@ -2,11 +2,11 @@ from argparse import ArgumentParser, Namespace
 from os import listdir
 from os.path import isdir
 from pathlib import Path
-from typing import List
+from typing import Hashable, List
 
 import pandas
 from matplotlib import pyplot as plt
-from pandas import DataFrame
+from pandas import DataFrame, Series
 from pandas.core.groupby.generic import DataFrameGroupBy
 from pandas.errors import ParserError
 from progress.bar import Bar
@@ -21,6 +21,22 @@ def getArgs() -> Namespace:
         required=True,
         help="Path to CSV file to count repositories OR directory containing CSVs",
     )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=Path,
+        required=False,
+        default="data.csv",
+        help="Path to store output CSV if analyzing a directory of files",
+    )
+    parser.add_argument(
+        "-f",
+        "--fig",
+        type=Path,
+        required=False,
+        default="barChart.png",
+        help="Path to store output figure if analyzing a directory of files",
+    )
     return parser.parse_args()
 
 
@@ -30,7 +46,27 @@ def groupByRepository(df: DataFrame) -> DataFrameGroupBy:
 
 
 def plot(df: DataFrame, outputFilePath: Path) -> None:
-    df.plot(kind="bar")
+    df.plot(kind="bar", x="library", y="projectCount", logy=True, legend=None)
+
+    plt.ylabel(ylabel="# of Projects")
+    plt.xlabel(xlabel="Library Name")
+    plt.xticks(rotation=45, ha="right", fontsize="x-small")
+    plt.title(label="# of Projects per Library")
+
+    ax = plt.gca()
+    bars = ax.bar(df["library"], df["projectCount"])
+
+    for bar in bars:
+        height = bar.get_height()
+        ax.annotate(
+            f"{height}",
+            xy=(bar.get_x() + bar.get_width() / 2, height),
+            xytext=(0, 1),
+            fontsize="xx-small",
+            textcoords="offset points",
+            ha="center",
+            va="bottom",
+        )
 
     plt.tight_layout()
     plt.savefig(outputFilePath)
@@ -58,9 +94,10 @@ def main() -> None:
         df: DataFrame = DataFrame(data=data).sort_values(
             by="projectCount", ignore_index=True
         )
-        df.to_csv(path_or_buf="projectCountOfAllCSVs.csv", index=False)
 
-        plot(df=df, outputFilePath=Path("test.png"))
+        df.to_csv(path_or_buf=args.output, index=False)
+
+        plot(df=df, outputFilePath=args.fig)
 
     else:
         df: DataFrame = pandas.read_csv(filepath_or_buffer=args.input)
