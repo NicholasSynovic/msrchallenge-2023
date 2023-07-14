959b763ebaf264d6cbf1c8829c122a23e228010b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-07-14 12:10:40 -0500

    Add command line arguements

--- msrchallenge_2023/compareCSVProjectCounts.py
+++ msrchallenge_2023/compareCSVProjectCounts.py
@@ -1,26 +1,37 @@
+from argparse import ArgumentParser, Namespace
+from typing import Path
+
+import pandas
 from pandas import DataFrame
 from pandas.core.groupby.generic import DataFrameGroupBy
-import pandas
+
+
+def getArgs() -> Namespace:
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=Path,
+        required=True,
+        help="Path to CSV file to count repositories",
+    )
+    return parser.parse_args()
+
 
 def groupByRepository(df: DataFrame) -> DataFrameGroupBy:
     dfgb: DataFrameGroupBy = df.groupby(by=["repository"])
     return dfgb
 
 
-def main()  ->  None:
-    transformersFilePath: str = "githubRepositoriesThatUseTransformersLibrary.csv"
-    spacyFilePath: str = "githubRepositoriesThatUseSpacyLibrary.csv"
+def main() -> None:
+    args: Namespace = getArgs()
 
-    transformersDF: DataFrame = pandas.read_csv(filepath_or_buffer=transformersFilePath)
-    spacyDF: DataFrame = pandas.read_csv(filepath_or_buffer=spacyFilePath)
+    df: DataFrame = pandas.read_csv(filepath_or_buffer=args.input)
 
-    transformersDFGB: DataFrameGroupBy = groupByRepository(df=transformersDF)
-    spacyDFGB: DataFrameGroupBy = groupByRepository(df=spacyDF)
+    dfgb: DataFrameGroupBy = groupByRepository(df=df)
 
-    print(f"Projects that use Transformers: {transformersDFGB.ngroups}")
-    print(f"Projects that use Spacy:        {spacyDFGB.ngroups}")
+    print(f"Project count: {dfgb.ngroups}")
 
-    pass
 
 if __name__ == "__main__":
     main()
