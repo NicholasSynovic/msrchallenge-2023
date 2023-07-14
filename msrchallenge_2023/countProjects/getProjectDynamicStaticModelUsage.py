ddba854c61b3d730c86c289b8dda057cf9a9326d
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-07-14 14:25:07 -0500

    Add code to print data about specific libraries

--- msrchallenge_2023/countProjects/getProjectDynamicStaticModelUsage.py
+++ msrchallenge_2023/countProjects/getProjectDynamicStaticModelUsage.py
@@ -4,14 +4,50 @@ import pandas
 from pandas import DataFrame, Series
 from pandas.core.groupby.generic import DataFrameGroupBy
 from progress.bar import Bar
+from argparse import Namespace, ArgumentParser
+from pathlib import Path
+from os import listdir
+from os.path import isdir
+from typing import List
+
+
+def getArgs() -> Namespace:
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=Path,
+        required=True,
+        help="Path to CSV file to count repositories OR directory containing CSVs",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=Path,
+        required=False,
+        default="data.csv",
+        help="Path to store output CSV if analyzing a directory of files",
+    )
+    parser.add_argument(
+        "-f",
+        "--fig",
+        type=Path,
+        required=False,
+        default="barChart.png",
+        help="Path to store output figure if analyzing a directory of files",
+    )
+    return parser.parse_args()
 
 
 def strReplacement(series: Series) -> Series:
-    series = series.str.replace(
-        r"[^a-zA-Z0-9\-\_\/]",
-        "",
-        regex=True,
-    )
+    try:
+        series = series.str.replace(
+            r"[^a-zA-Z0-9\-\_\/]",
+            "",
+            regex=True,
+        )
+    except AttributeError:
+        pass
 
     return series
 
@@ -69,9 +105,13 @@ def countDynamicModelUsageRepositories(
     percentPureStaticUsage_total: float = (
         pureStaticUsageRepos / totalNumberOfRepos
     ) * 100
-    percentPureStaticUsage_partial: float = (
-        pureStaticUsageRepos / staticUsageRepos
-    ) * 100
+    
+    try:
+        percentPureStaticUsage_partial: float = (
+            pureStaticUsageRepos / staticUsageRepos
+        ) * 100
+    except ZeroDivisionError:
+        percentPureStaticUsage_partial: float = 0
 
     return (
         totalNumberOfRepos,
@@ -86,19 +126,59 @@ def countDynamicModelUsageRepositories(
 
 
 def main() -> None:
-    csvFile: str = "githubRepositoriesThatUseTransformersLibrary.csv"
+    args: Namespace = getArgs()
 
-    df: DataFrame = pandas.read_csv(filepath_or_buffer=csvFile)
+    if isdir(s=args.input):
+        dataDict: dict[str, List[str | int | float]] = {
+            "library": [],
+            "projectCount": [],
+            "dynamicUsageCount": [],
+            "patialStaticUsageCount": [],
+            "pureStaticUsageCount": [],
+            "percentDynamicUsage": [],
+            "percentPartialStaticUsage": [],
+            "percentPureStaticUsage_partial": [],
+            "percentPureStaticUsage_total": [],
+        }
 
-    df["param_hardcoded"] = strReplacement(series=df["param_hardcoded"])
+        fileList: List[Path] = [Path(args.input, f) for f in listdir(path=args.input)]
 
-    dfgb: DataFrameGroupBy = groupByRepository(df=df)
+        file: Path
+        for file in fileList:
+            dataDict["library"].append(file.stem)
 
-    data: tuple[int, int, int, float, float] = countDynamicModelUsageRepositories(
-        dfgb=dfgb
-    )
+            df: DataFrame = pandas.read_csv(filepath_or_buffer=file)
+            df["param_hardcoded"] = strReplacement(series=df["param_hardcoded"])
+            dfgb: DataFrameGroupBy = groupByRepository(df=df)
+    
+            data: tuple[
+                int, int, int, int, float, float, float, float
+            ] = countDynamicModelUsageRepositories(dfgb=dfgb)
 
-    print(data)
+            dataDict["projectCount"].append(data[0])
+            dataDict["dynamicUsageCount"].append(data[1])
+            dataDict["patialStaticUsageCount"].append(data[2])
+            dataDict["pureStaticUsageCount"].append(data[3])
+            dataDict["percentDynamicUsage"].append(data[4])
+            dataDict["percentPartialStaticUsage"].append(data[5])
+            dataDict["percentPureStaticUsage_partial"].append(data[6])
+            dataDict["percentPureStaticUsage_total"].append(data[7])
+
+        df: DataFrame = DataFrame(data=dataDict).sort_values(
+            by="projectCount", ignore_index=True
+        )
+
+        print(df)
+
+    else:
+        csvFile: str = args.input
+        df: DataFrame = pandas.read_csv(filepath_or_buffer=csvFile)
+        df["param_hardcoded"] = strReplacement(series=df["param_hardcoded"])
+        dfgb: DataFrameGroupBy = groupByRepository(df=df)
+        data: tuple[
+            int, int, int, int, float, float, float, float
+        ] = countDynamicModelUsageRepositories(dfgb=dfgb)
+        print(data)
 
 
 if __name__ == "__main__":
