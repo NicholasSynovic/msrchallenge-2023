d676903db2e0ce5329857df5419e4741ede2df07
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-11-09 16:06:53 -0600

    Updated code

--- msrchallenge_2023/countProjects/countProjectsInCSV_new.py
+++ msrchallenge_2023/countProjects/countProjectsInCSV_new.py
@@ -1,18 +1,14 @@
-from argparse import ArgumentParser, Namespace
-from os import listdir
-from os.path import isdir
 from pathlib import Path
-from typing import Hashable, List
 
 import pandas
 from matplotlib import pyplot as plt
-from pandas import DataFrame, Series
-from pandas.core.groupby.generic import DataFrameGroupBy
-from pandas.errors import ParserError
-from progress.bar import Bar
+from matplotlib.axes import Axes
+from pandas import DataFrame
 
 
 def plot(df: DataFrame, outputFilePath: Path) -> None:
+    labelFontSize: int = 18
+
     df.plot(
         kind="bar",
         x="library",
@@ -20,19 +16,20 @@ def plot(df: DataFrame, outputFilePath: Path) -> None:
         logy=True,
         legend=None,
         figsize=(10, 5),
-        ylim=(1, 25000),
     )
 
-    plt.ylabel(ylabel="# of Projects")
-    plt.xlabel(xlabel="Library Name")
+    plt.ylabel(ylabel="# of Projects", fontsize=labelFontSize)
+    plt.xlabel(xlabel="Library Name", fontsize=labelFontSize)
     plt.xticks(rotation=45, ha="right", fontsize="large")
 
-    ax = plt.gca()
+    ax: Axes = plt.gca()
     bars = ax.bar(
         df["library"],
         df["projectCount"],
         align="center",
     )
+
+    ax.yaxis.set_major_formatter
 
     for bar in bars:
         height = bar.get_height()
@@ -40,20 +37,20 @@ def plot(df: DataFrame, outputFilePath: Path) -> None:
             f"{height}",
             xy=(bar.get_x() + bar.get_width() / 2, height),
             xytext=(0, 1),
-            fontsize="medium",
+            fontsize=10,
             textcoords="offset points",
             ha="center",
             va="bottom",
         )
 
-    print(plt.rcParams["figure.figsize"])
+    plt.margins(y=0.08, tight=True)
     plt.tight_layout()
+    plt.xlim(left=-0.8)
     plt.savefig(outputFilePath)
 
 
 def main() -> None:
     df: DataFrame = pandas.read_csv(filepath_or_buffer="newData.csv")
-    print(df.columns)
     df.sort_values(by="projectCount", axis=0, ascending=False, inplace=True)
     plot(df=df, outputFilePath=Path("test2.pdf"))
 
